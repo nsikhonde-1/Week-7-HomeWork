a7cd3d854c523c91ac55c91f8d8f82c16e6c8f38
nsikhonde-1 <nsikhonde1@gmail.com>
2025-02-02 14:33:54 +0200

    Adding functions

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-let searchForm = document.querySelector("#form-box");
-searchForm.addEventListener("submit", searchHandle);
 
 function searchHandle(event) {
     event.preventDefault(); 
     let searchInput = document.querySelector("#city");
-    let cityName=document.querySelector("h1")
-    cityName.innerHTML = searchInput.value;
+    let city = searchInput.value;
 }
+let searchForm = document.querySelector(".form-box");
+searchForm.addEventListener("submit", searchHandle);
 
 let now = new Date();
 let hour = now.getHours();
@@ -18,3 +17,30 @@ let day = days[now.getDay()]
    p.innerHTML = `${day}`;
    let watch = document.querySelector("#time");
    watch.innerHTML = `${hour}:${minutes}`;
+
+let apiKey = "713a631324o2dtc013aaf4374b846544";
+let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=713a631324o2dtc013aaf4374b846544&units=metric";
+axios.get(apiUrl).then(displayTemperature);
+
+function displayTemperature(response) {
+    console.log(response)
+    let cityName = document.querySelector(".place");
+    cityName.innerHTML = response.data.city;
+
+    let temperature = Math.round(response.data.temperature.current);
+    let description = response.data.condition.description;
+    let humidity = response.data.temperature.humidity;
+    let wind = response.data.wind.speed;
+
+    let temp = document.querySelector(".weather-temp-number");
+    temp.innerHTML = `${temperature}`
+
+    let explain = document.querySelector(".detail");
+    explain.innerHTML = `${description}`;
+
+    let damp=document.querySelector(".percentage")
+    damp.innerHTML = `${humidity}%`
+    
+    let windSpeed = document.querySelector(".km")
+    windSpeed.innerHTML=`${wind}km/h`
+}
